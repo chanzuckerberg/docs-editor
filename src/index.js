2a0d98fdb0e95e4bc78b5302c0b97db8b359e981
Hedger Wang <hedgerwang+github@gmail.com>
2018-07-14 00:50:47 -0700

    export `Timer`.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ export {default as DocsActionTypes} from './DocsActionTypes';
 export {default as DocsContext} from './DocsContext';
 export {default as DocsEditor} from './DocsEditor';
 export {default as DocsImageUploadControl} from './DocsImageUploadControl';
+export {default as Timer} from './Timer';
 export {default as captureDocumentEvents} from './captureDocumentEvents';
 export {default as convertFromRaw} from './convertFromRaw';
 export {default as docsWithContext} from './docsWithContext';
 export {default as showModalDialog} from './showModalDialog';
 export {default as uniqueID} from './uniqueID';
-
 export {convertToRaw, EditorState} from 'draft-js';
 export {isEditorStateEmpty} from './DocsHelpers';
 
