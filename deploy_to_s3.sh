1b3f1e6d39c0d8af05a81c808d7a0835ff993aa7
Hedger Wang <hedgerwang+github@gmail.com>
2018-07-17 10:46:32 -0700

    trim dependencies.

--- deploy_to_s3.sh
+++ deploy_to_s3.sh
@@ -1,7 +1,7 @@
-# namespace: education_doc_editor_0_0_1_4
-# version: 0.0.1
+# namespace: education_doc_editor_0_0_2_4
+# version: 0.0.2
 # name: education-doc-editor
 # subversion: 4
 
-# http://cdn.summitlearning.org/assets/examples_education_doc_editor_0_0_1_4.html
-aws s3 cp deploy/examples_education_doc_editor_0_0_1_4.html s3://opt-static-resources/assets/examples_education_doc_editor_0_0_1_4.html --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers;
+# http://cdn.summitlearning.org/assets/index_education_doc_editor_0_0_2_4.html
+aws s3 cp deploy/index_education_doc_editor_0_0_2_4.html s3://opt-static-resources/assets/index_education_doc_editor_0_0_2_4.html --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers;
