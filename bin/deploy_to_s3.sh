1e57bc20fd572d78415d77b7fe41a28dde389c10
Hedger Wang <hedgerwang+github@gmail.com>
2018-08-23 21:21:20 -0700

    rename package name to `docs-editor`.

--- bin/deploy_to_s3.sh
+++ bin/deploy_to_s3.sh
@@ -1,10 +1,10 @@
-# namespace: doc_editor_0_0_4_6
-# version: 0.0.4
-# name: doc-editor
+# namespace: docs_editor_0_0_5_6
+# version: 0.0.5
+# name: docs-editor
 # subversion: 6
 
 # http://cdn.summitlearning.org/assets/index_latest.html
 aws s3 cp bin/index_latest.html s3://opt-static-resources/assets/index_latest.html --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers;
 
-# http://cdn.summitlearning.org/assets/index_doc_editor_0_0_4_6.html
-aws s3 cp bin/index_doc_editor_0_0_4_6.html s3://opt-static-resources/assets/index_doc_editor_0_0_4_6.html --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers;
+# http://cdn.summitlearning.org/assets/index_docs_editor_0_0_5_6.html
+aws s3 cp bin/index_docs_editor_0_0_5_6.html s3://opt-static-resources/assets/index_docs_editor_0_0_5_6.html --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers;
