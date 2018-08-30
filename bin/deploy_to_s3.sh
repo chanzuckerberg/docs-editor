1a328dc8cef47b075443c747ead1791244b6cf92
Hedger Wang <hedgerwang+github@gmail.com>
2018-08-29 22:02:29 -0700

    build files.

--- bin/deploy_to_s3.sh
+++ bin/deploy_to_s3.sh
@@ -1,10 +1,10 @@
-# namespace: docs_editor_0_0_5_6
+# namespace: docs_editor_0_0_5_7
 # version: 0.0.5
 # name: docs-editor
-# subversion: 6
+# subversion: 7
 
 # http://cdn.summitlearning.org/assets/index_latest.html
 aws s3 cp bin/index_latest.html s3://opt-static-resources/assets/index_latest.html --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers;
 
-# http://cdn.summitlearning.org/assets/index_docs_editor_0_0_5_6.html
-aws s3 cp bin/index_docs_editor_0_0_5_6.html s3://opt-static-resources/assets/index_docs_editor_0_0_5_6.html --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers;
+# http://cdn.summitlearning.org/assets/index_docs_editor_0_0_5_7.html
+aws s3 cp bin/index_docs_editor_0_0_5_7.html s3://opt-static-resources/assets/index_docs_editor_0_0_5_7.html --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers;
