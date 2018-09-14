820d498acb74e8120120603158d30bc51bc0fc5b
Hedger Wang <hedgerwang+github@gmail.com>
2018-09-14 11:13:36 -0700

    fix `isEditorStateEmpty`

--- src/isEditorStateEmpty.js
+++ src/isEditorStateEmpty.js
@@ -3,7 +3,19 @@
 import {EditorState} from 'draft-js';
 
 function isEditorStateEmpty(editorState: EditorState): boolean {
-  return !editorState.getCurrentContent().hasText();
+  const contentState = editorState.getCurrentContent();
+  const hasText = contentState.hasText();
+  if (hasText) {
+    return false;
+  }
+  const blockMap = contentState.getBlockMap();
+  if (blockMap.size > 1) {
+    return false;
+  }
+  if (blockMap.size === 1) {
+    return blockMap.first().getType() === 'unstyled';
+  }
+  return true;
 }
 
 export default isEditorStateEmpty;
